bb1ec2e98ef4dd454667d28e128d40e24377733b
Mücahit <46865744+BagBaq@users.noreply.github.com>
2022-07-20 18:36:34 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,29 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 
 shared_scripts { 
-	'config/config.lua',
+	'config.lua'
 }
 
 server_scripts {
-    'server/wrappers/business.lua',
-    'server/wrappers/useraccounts.lua',
-    'server/wrappers/gangs.lua',
-    'server/main.lua'
+    'server.lua'
 }
 
 client_scripts {
-    'client/main.lua',
-    'client/nui.lua'
-}
-
-ui_page 'nui/index.html'
-
-files {
-    'nui/images/logo.png',
-    'nui/scripting/jquery-ui.css',
-    'nui/scripting/external/jquery/jquery.js',
-    'nui/scripting/jquery-ui.js',
-    'nui/style.css',
-    'nui/index.html',
-    'nui/qb-banking.js',
+    'client.lua'
 }
